0355522bdf34dedfd21e29b441553079a82daab0
Matthew Cook <mcook@arrowheaddental.com>
2013-07-10 04:16:04 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '60a73efd93769b4e4b1ac26f96be660d7b056792235b7ca1aedad5430bfa3bf9236641c0b702691619e2500cf62a4260bcfd61db3ca9fd11be843864f5b54f2c'
