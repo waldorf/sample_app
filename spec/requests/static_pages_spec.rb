230d2cc29ca275772acd93f5b44ac3cd846188f7
Matthew Cook <mcook@arrowheaddental.com>
2013-07-21 22:08:16 -0600

    End of Chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:pgtitle) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home Page" do
 
     it "should have the h1 'Sample App' " do
@@ -10,10 +12,18 @@ describe "StaticPages" do
     end
 
 
-  	it "should have the title 'Home' " do
+  	it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                            :text => "#{pgtitle}")
+    end
+
+
+
+    it "should not have a custom page title 'Home' " do
   		visit '/static_pages/home'
-  		page.should have_selector('title',
-                          :text => "Ruby on Rails Tutorial Sample App | Home")
+  		page.should_not have_selector('title',
+                          :text => "| Home")
   	end
   end
 
@@ -27,7 +37,7 @@ describe "StaticPages" do
     it "should have the title 'Help' " do
   		visit '/static_pages/help'
   		page.should have_selector('title',
-                          :text => "Ruby on Rails Tutorial Sample App | Help")
+                          :text => "#{pgtitle} | Help")
   	end
   end
 
@@ -41,7 +51,21 @@ describe "StaticPages" do
     it "should have the title 'About Us' " do
       visit '/static_pages/about'
       page.should have_selector('title',
-                          :text => "Ruby on Rails Tutorial Sample App | About Us")
+                          :text => "#{pgtitle} | About Us")
+    end
+  end
+
+  describe "Contact Page" do
+
+    it "should have the h1 'Contact' " do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact' " do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                          :text => "#{pgtitle} | Contact")
     end
   end
 
